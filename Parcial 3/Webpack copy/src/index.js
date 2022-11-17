c3f2ab765578896217afbef02d0b52bbede11580
nayaracasz <nayaracs208@gmail.com>
2022-11-17 06:38:08 -0600

    add tareas

---
+++ Parcial 3/Webpack copy/src/index.js
@@ -0,0 +1,9 @@
+// const modulo = require('./modulo.js');
+
+// console.log(modulo.cuadrado(5));
+
+document.getElementById("alerta").addEventListener("click", ()=>{
+    swal("Good job!", "You clicked the button!", "success", {
+        button: "Aww yiss!",
+    });
+});
